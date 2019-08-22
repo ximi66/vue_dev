b5eae076f6600dda4d163a6e1b7bbc22b1ea45af
xiamgmiao <xiangmiao@motherplanet.cn>
2019-08-22 11:17:57 +0800

    删去无用代码

--- src/api/about.js
+++ src/api/about.js
@@ -1,15 +1,5 @@
 import axios from "@/axios/axiosConfig";
 
-export function detailApi(param) {
-  return axios({
-    url: `/dsc/sys/main-info/detail/${param.applyNo}`,
-    headers: {
-      "Content-Type": "application/json;charset=UTF-8"
-    },
-    method: "get"
-  });
-}
-
 export function synLogInfoApi(param) {
   return axios({
     url: "/api/data",
